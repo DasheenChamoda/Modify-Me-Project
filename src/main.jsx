cdd128b0a86baf276aea426d6663d83307c3b5df
TharinduRupasinghe <tharindu.rupasinghe926@gmail.com>
2024-03-20 22:46:00 +0530

    Add Shop Page

--- src/main.jsx
+++ src/main.jsx
@@ -33,6 +33,10 @@ const router = createBrowserRouter([
       {
         path: "/about",
         element:<About/>
+      },
+      {
+        path: "/shop",
+        element:<Shop/>
       }
     ]
   },
