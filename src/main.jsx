6ee9ebb77d4d8ba8bda235ee5624034b7af16576
LAPTOP-1DBO4K9R\dilmi <dilminakru@gmail.com>
2024-03-21 04:26:52 +0530

    added some products

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,7 @@ import {
 } from "react-router-dom";
 import Home from './Home/Home.jsx';
 import About from './about/About.jsx';
-//import Shop from './shop/Shop.jsx';
+import Shop from './shop/Shop.jsx';
 
 const router = createBrowserRouter([
   {
@@ -35,10 +35,10 @@ const router = createBrowserRouter([
         path: "/about",
         element:<About/>
       },
-      /*{
+      {
         path: "/shop",
         element:<Shop/>
-      }*/
+      }
     ]
   },
 ]);
