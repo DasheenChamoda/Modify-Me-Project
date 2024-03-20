1cd5ad665537b737cce5de5a54231373cb0f7afb
DasheenChamoda <150607815+DasheenChamoda@users.noreply.github.com>
2024-03-20 11:47:17 +0530

    Vuforia License added

---
+++ Rebuild-New/Rebuild/Assets/Assets/Scripts/Script_Buttons.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Script_Buttons : MonoBehaviour
+{
+
+    public string description;
+    public TextMeshProUGUI textBox;
+    public GameObject PARscene;
+    public GameObject wlcanvas;
+    public GameObject blureffect;
+  
+    void Start()
+    {
+        
+    }
+ 
+    void Update()
+    {
+        
+    }
+}
